90a3335f2008841d310bbd77eec1e23f55239fb7
Tharunn D <83963600+tharunn2007@users.noreply.github.com>
2026-05-12 13:12:46 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -36,6 +36,48 @@ A YEAR IS NOT LEAP YEAR IF:
  */
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+
+public class practice {
+    static int Sum(int a,int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number 1 :");
+        Integer a = input.nextInt();
+        System.out.println("Enter number 2 :");
+        Integer b = input.nextInt();
+
+        System.out.println("The sum of a and b is : "+ Sum(a,b));
+    }
+    
+    
+}
+/*TAKE INPUT FROM USER AND USE A SUM METHOD */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+
+public class practice {
+    static void MultiplicationTable(int number,int tonumber) { 
+        for(int i = 1 ; i<=tonumber;i++){
+           
+            System.out.println(number+ " * " + tonumber + " = "+ (number*i));
+        } //need not write return cuz void doesnt return anything
+
+    }
+    public static void main(String[] args) { //while writing void we dont need to return and also return type (int,boolean,etc) need not be mentioned
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number to be multiplied : ");
+        Integer number = input.nextInt();
+        System.out.println("Enter the number to which to be multiplied ; ");
+        Integer tonumber = input.nextInt();
+        
+        MultiplicationTable(number, tonumber); //need not print when we are calling for void based method
+    }
+    
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
